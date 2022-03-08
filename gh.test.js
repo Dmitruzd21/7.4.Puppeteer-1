2cf719de82d569f45837813c7fb2749a617ac856
Korotkov Dmitry <2110821@mail.ru>
2022-03-08 18:44:31 +0300

    New hook

--- gh.test.js
+++ gh.test.js
@@ -1,15 +1,14 @@
 let page;
-let anotherPage;
 
- describe("Github page tests", () => {
+afterEach(async() => {
+   await page.close();
+ });
+
+describe("Github page tests", () => {
   beforeEach(async () => {
    page = await browser.newPage();
    await page.goto("https://github.com/team");
 });
-
-  afterEach(async() => {
-   await page.close();
- });
 
   test("The h1 header content'", async () => {
     await page.setDefaultTimeout(3000);
@@ -39,16 +38,12 @@ let anotherPage;
 
 describe("Securuty Page", () => {
   beforeEach(async () => {
-    anotherPage = await browser.newPage();
-    await anotherPage.goto("https://github.com/features/security");
-  });
-
-  afterEach(async() => {
-    await anotherPage.close();
+    page = await browser.newPage();
+    await page.goto("https://github.com/features/security");
   });
   
   test("Text of Header Buttom", async () => {
-     const headerElement = await anotherPage.$(".color-shadow-small.js-sticky.w-full.z-3 > div > a");
+     const headerElement = await page.$(".color-shadow-small.js-sticky.w-full.z-3 > div > a");
      const elementText = await headerElement.evaluate (el => el.textContent);
      expect(elementText).toEqual("Security");
   });
@@ -56,7 +51,7 @@ describe("Securuty Page", () => {
   test("h1 Text", async () => {
     await page.setDefaultTimeout(5000);
     const h1 = await "h1.h1-mktg.mb-4";
-    const h1Text = await anotherPage.$eval(h1, el => el.textContent);
+    const h1Text = await page.$eval(h1, el => el.textContent);
     const h1TextAfterTransform1 = await h1Text.slice(0,15);
     const h1TextAfterTransform2 = await h1Text.slice(16);
     const h1TextAfterJoin = [h1TextAfterTransform1, h1TextAfterTransform2].join(" ");
@@ -65,7 +60,7 @@ describe("Securuty Page", () => {
 
   test("h4Span Text under h1", async () => {
     const h4Span = await "h4 span.color-fg-default";
-    const h4SpanText = await anotherPage.$eval(h4Span, el => el.textContent);
+    const h4SpanText = await page.$eval(h4Span, el => el.textContent);
     expect(h4SpanText).toEqual("Ship secure applications within the GitHub flow");
   });
 });
